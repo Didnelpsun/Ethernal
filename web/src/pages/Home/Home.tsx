91cdabd62fd7e7797672b1371410e734ea93cfb9
Didnelpsun <267350965@qq.com>
2019-12-20 01:04:25 +0800

    updates

--- web/src/pages/Home/Home.tsx
+++ web/src/pages/Home/Home.tsx
@@ -4,64 +4,106 @@ import './Home.css'
 import '../../assets/styles/theme/DawnGolden/DawnGolden.css'
 import YellowButton from '../../components/ColorButton/ColorButton'
 import { Link } from 'react-router-dom'
+import { Icon } from 'antd'
 
 const style1 = {
-  width: '200px',
-  height: '75px',
-  fontSize: '30px',
+  width: '150px',
+  height: '55px',
+  fontSize: '25px',
   backgroundColor: '#fae379',
   color: 'white'
 }
 
 const style2 = {
-  width: '200px',
-  height: '75px',
-  fontSize: '30px',
+  width: '150px',
+  height: '55px',
+  fontSize: '25px',
   backgroundColor: 'white',
   color: '#fae379'
 }
 
-let style: any = {
-  color: '',
-};
+let styleshow = { display: '' };
+let stylenone = { display: 'none' };
 
-function changeColor1() {
-  const signup: HTMLDivElement = document.getElementById('signup') as HTMLDivElement;
-  signup.style.color = 'white';
+function changeColor(props: string) {
+  const div: HTMLDivElement = document.getElementById(props) as HTMLDivElement;
+  div.style.color = div.style.color == '' ? 'white' : '';
 }
 
-function changeBack1() {
-  const signup: HTMLDivElement = document.getElementById('signup') as HTMLDivElement;
-  signup.style.color = '';
+function changeButton(name: string, color: string) {
+  const button: HTMLButtonElement = document.getElementById(name) as HTMLButtonElement;
+  button.style.backgroundColor = button.style.backgroundColor == '' ? color : '';
 }
-
-function changeColor2() {
-  const showmore: HTMLDivElement = document.getElementById('showmore') as HTMLDivElement;
-  showmore.style.color = 'white';
+interface InState {
+  styleuser: object;
+  stylesignin: object
 }
+class Home extends React.Component<{}, InState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      styleuser: styleshow,
+      stylesignin: stylenone
+    }
+  }
 
-function changeBack2() {
-  const showmore: HTMLDivElement = document.getElementById('showmore') as HTMLDivElement;
-  showmore.style.color = '';
-}
+  signinButton() {
+    if (this.state.styleuser == styleshow) {
+      this.setState({
+        styleuser: stylenone,
+        stylesignin: styleshow
+      })
+    }
+    else {
+      this.setState({
+        styleuser: styleshow,
+        stylesignin: stylenone
+      })
+    }
+  }
 
-class Home extends React.Component {
   render() {
     return (
       <div className='Main'>
-        <Link to='/login/' style={{ textDecoration: 'none' }}>
-          <YellowButton
-            content='Log In'
-            style1={style1}
-            style2={style2}
-          />
-        </Link>
-        <Link to='/signup/' style={{ textDecoration: 'none' }}>
-          <div className='MainMessage' id='signup' onMouseEnter={changeColor1} onMouseLeave={changeBack1}>Sign Up</div>
-        </Link>
-        <Link to='/more/' style={{ textDecoration: 'none' }}>
-          <div className='MainMessage' id='showmore' onMouseEnter={changeColor2} onMouseLeave={changeBack2}>Show Me More</div>
-        </Link>
+        <div className='header'>
+          <div className='canvas'>
+            <span className='title'>Ethernal Intelligence</span>
+          </div>
+          <div className='userboard'>
+            <div className='user' style={this.state.styleuser}>
+              <div onClick={this.signinButton.bind(this)}>
+                <YellowButton
+                  content='Sign In'
+                  style1={style1}
+                  style2={style2}
+                />
+              </div>
+              <Link to='/signup/' style={{ textDecoration: 'none' }}>
+                <div className='MainMessage' id='signup' onMouseEnter={changeColor.bind(this, 'signup')} onMouseLeave={changeColor.bind(this, 'signup')}>Sign Up</div>
+              </Link>
+              <Link to='/more/' style={{ textDecoration: 'none' }}>
+                <div className='MainMessage' id='showmore' onMouseEnter={changeColor.bind(this, 'showmore')} onMouseLeave={changeColor.bind(this, 'showmore')}>Show Me More</div>
+              </Link>
+            </div>
+            <div className='signupboard' style={this.state.stylesignin}>
+              <form className='content'>
+                <span>Username</span>
+                <div style={{ flexDirection: 'row' }}>
+                  <input></input>
+                </div>
+                <span>Password</span>
+                <div style={{ flexDirection: 'row' }}>
+                  <input type='password'></input>
+                </div>
+                <button className='submit' id='submit' type='submit' onMouseEnter={changeButton.bind(this,'submit','#01c501')} onMouseOut={changeButton.bind(this,'submit','#01c501')}>Submit And Sign Up</button>
+                <button className='reset' id='reset' type='reset' onClick={this.signinButton.bind(this)} onMouseEnter={changeButton.bind(this,'reset','red')} onMouseOut={changeButton.bind(this,'reset','red')}>Reset And Turn Back</button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className='footer'>
+
+        </div>
       </div>
     );
   }
